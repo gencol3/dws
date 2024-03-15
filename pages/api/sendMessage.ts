1337f8a52f2a0ecbf29752ce2ed486cc367ab258
clover00000011 <fclover996@gmail.com>
2024-03-14 22:50:04 -0700

    first commit

--- pages/api/sendMessage.ts
+++ pages/api/sendMessage.ts
@@ -13,9 +13,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     console.log('Received form data:', formData);
 
-    resend.emails.send({
+    await resend.emails.send({
       from: 'dws@resend.dev',
-      to: ['delivered@resend.dev'],
+      to: 'digitalwonderssolutions@gmail.com',
       subject: 'New Contact Form Message',
       html: `<p>This is an Email to DWS from Contact Form. the name of the person is: ${formData.name}, the referrer is ${formData.referrer}, and the email is ${formData.email}. The message is: (${formData.message})</p>`
     });
