5b25e98ef521b126224aad19aa90d43400885c89
gencol3 <160202993+gencol3@users.noreply.github.com>
2024-03-30 01:46:07 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={truculenta.className}>{children}</body>
-      <Script (function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('ywVkO4XBBBE8Z6Bj'); />
+      <Script id="leadfeeder-script" strategy="lazyOnload">
+        {`
+          (function(ss,ex){
+            window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));};
+            (function(d,s){
+              fs=d.getElementsByTagName(s)[0];
+              function ce(src){
+                var cs=d.createElement(s);
+                cs.src=src;
+                cs.async=1;
+                fs.parentNode.insertBefore(cs,fs);
+              };
+              ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js');
+            })(document,'script');
+          })('ywVkO4XBBBE8Z6Bj');
+        `}
+      </Script>
     </html>
   );
 }
