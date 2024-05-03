f6a35e5bad14ccf4c32c21372c2c9b9d25d4859c
gencol3 <160202993+gencol3@users.noreply.github.com>
2024-05-03 13:57:28 -0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'custom-blue':'#070F2B',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
